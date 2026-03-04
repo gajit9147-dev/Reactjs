c955626c26bc02ab75082f9454b9023a10bcddd3
gajit9147 <gajit9147@gmail.com>
2026-03-04 22:30:50 +0530

    pass UserContextProvder

--- ReactLearning/ContextAPI/src/App.jsx
+++ ReactLearning/ContextAPI/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
+import UserContextProvider from './context/userContextProvider'
 
 function App() {
   return (
-    <>
+    < UserContextProvider>
       <h3 className="text-3xl font-bold underline">
         React context API Ajeet
       </h3>
-    </>
+    </UserContextProvider>
   )
 }
 
